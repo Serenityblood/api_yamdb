18ab89b17ed5e8bca913133c7659839a24e31aff
Сергей Юдаев <bat.ru2013@mail.ru>
2022-09-22 13:24:30 +0300

    Models work-in-progres

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -15,22 +15,21 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         read_only_field = ('role',)
 
-    class ReviewSerializer(serializers.ModelSerializer):
-        author = serializers.SlugRelatedField(
-            read_only=True, slug_field='username'
-        )
 
-        def validate(self, data):
-            if self.context['request'].method != 'POST':
-                return data
-            user = self.context['request'].user
-            title_id = self.context['view'].kwargs['title_id']
-            if Review.objects.filter(author=user, title_id=title_id).exists():
-                raise serializers.ValidationError(
-                    'Отзыв уже оставлен!'
-                )
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(read_only=True, slug_field='username')
+
+    def validate(self, data):
+        if self.context['request'].method != 'POST':
             return data
+        user = self.context['request'].user
+        title_id = self.context['view'].kwargs['title_id']
+        if Review.objects.filter(author=user, title_id=title_id).exists():
+            raise serializers.ValidationError(
+                'Отзыв уже оставлен!'
+            )
+        return data
 
-        class Meta:
-            fields = ('id', 'text', 'author', 'score', 'pub_date')
-            model = Review
+    class Meta:
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
+        model = Review
