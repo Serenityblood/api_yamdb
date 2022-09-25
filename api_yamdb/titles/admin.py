7c93ce2e72e035c91494dc26a4df918948a473cd
Сергей Юдаев <bat.ru2013@mail.ru>
2022-09-25 13:24:50 +0300

    Add admin class

--- api_yamdb/titles/admin.py
+++ api_yamdb/titles/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.modes import CustomUser
+from .models import Category, Genre, Title, Review, Comment
+
+
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'bio', 'role')
+    search_fields = ('username',)
+    list_filter = ('role',)
+    empty_value_display = '--пусто--'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
