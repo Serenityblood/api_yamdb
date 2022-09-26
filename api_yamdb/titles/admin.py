ad85d87e20283426d33711fd1f7709bb486b6db9
Глеб Бушланов <miracle_5@bk.ru>
2022-09-26 22:04:32 +0300

    Auth system done

--- api_yamdb/titles/admin.py
+++ api_yamdb/titles/admin.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
 
-from users.modes import CustomUser
 from .models import Category, Genre, Title, Review, Comment
-
-
-@admin.register(CustomUser)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email', 'bio', 'role')
-    search_fields = ('username',)
-    list_filter = ('role',)
-    empty_value_display = '--пусто--'
 
 
 @admin.register(Comment)
